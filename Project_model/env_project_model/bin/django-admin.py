4cd4646d2532b9dee0227e055fb4cf2e19596287
ammar2621 <ammar@alterra.id>
2019-07-22 11:19:29 +0700

    menambahkan tugas Django

---
+++ Project_model/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta4/Django_MVC/Django_MVC/Project_model/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
